ab6c0a51aa2d66237cbd635fa1b0abbb87c17aa2
David Klingler <deweydbb@gmail.com>
2024-05-05 15:54:38 -0500

    started on python productions

--- pythonLexer.py
+++ pythonLexer.py
@@ -1,11 +1,482 @@
 import ply.lex as lex
 
 pythonProductions = {
-    
+    "S": [
+        ['single_input'],
+        ['file_input'],
+        ['eval_input'],  
+    ],
+    "single_input": [
+        ['NEWLINE'],
+        ['simple_stmt'],
+        ['compound_stmt', 'NEWLINE'],
+    ],
+    "file_input": [
+        ['NEWLINE'],
+        ['stmt'],
+        ['file_input', 'NEWLINE'],
+        ['file_input', 'stmt'],
+    ],
+    "eval_input": [
+        ['testlist', 'NEWLINE'],
+    ],
+
+    "decorator": [
+        ['@', 'dotted_name', 'NEWLINE'],
+        ['@', 'dotted_name', '(', 'arguments', ')', 'NEWLINE'],
+    ],
+    "decorators": [
+        ['decorator'],
+        ['decorators', 'decorator'],
+    ],
+    "decorated": [
+        ['decorators', 'classdef'],
+        ['decorators', 'funcdef'],
+        ['decorators', 'async_funcdef'],
+    ],
+
+    "async_funcdef": [
+        ['ASYNC', 'funcdef'],
+    ],
+    "funcdef": [
+        ['DEF', 'name', '(', ')', ':', 'suite'],
+        ['DEF', 'name', '(', 'parameters', ')', ':', 'suite'],
+        ['DEF', 'name', '(', ')', ':', 'RARROW', 'test', 'suite'],
+        ['DEF', 'name', '(', 'parameters', ')', ':', 'RARROW', 'test', 'suite'],
+    ],
+
+    "parameters": [
+        ['paramvalue', 'paramlist'],
+        ['paramvalue', 'paramlist', ',' '/', 'paramlist'],
+        ['paramvalue', 'paramlist', ',', 'starparams'],
+        ['paramvalue', 'paramlist', ',', 'kwparams'],
+        ['paramvalue', 'paramlist', ',' '/', 'paramlist', ',', 'starparams'],
+        ['paramvalue', 'paramlist', ',' '/', 'paramlist', ',', 'kwparams'],
+        ['starparams'],
+        ['kwparams']
+    ],
+    "paramlist": [
+        [],
+        [',', 'paramvalue', 'paramlist'],
+    ],
+
+    "starparams": [
+        ['starparam', 'poststarparams'],
+        ['starguard', 'poststarparams'],
+    ],
+    "starparam": [
+        ['*', 'typedparam'],
+        ['*'],
+    ],
+    "poststarparams": [
+        ['paramlist'],
+        ['paramlist', ',', 'kwparams'],
+    ],
+    "kwparams": [
+        ['**', 'typedparam'],
+        ['**', 'typedparam', ','],
+    ],
+
+    "paramvalue": [
+        ['typedparam'],
+        ['typedparam', '=', 'test'],
+    ],
+    "typedparam": [
+        ['name'],
+        ['name', ':', 'test'],
+    ],
+
+    "lambdef": [
+        ['LAMBDA', ':', 'test'],
+        ['LAMBDA', 'lambda_params', ':', 'test'],
+    ],
+    "lambdef_nocond": [
+        ['LAMBDA', ':', 'test_nocond'],
+        ['LAMBDA', 'lambda_params', ':', 'test_nocond'],
+    ],
+    "lambda_params": [
+        ['lambda_paramvalue', 'lambda_paramlist'],
+        ['lambda_paramvalue', 'lambda_paramlist', ',', 'lambda_starparams'],
+        ['lambda_paramvalue', 'lambda_paramlist', ',', 'lambda_kwparams'],
+    ],
+    "lambda_paramlist": [
+        [],
+        [',', 'lambda_paramvalue', 'lambda_paramlist'],
+    ],
+    "lambda_paramvalue": [
+        ['name'],
+        ['name', '=', 'test'],
+    ],
+    "lambda_starparams": [
+        ['*', 'lambda_paramlist'],
+        ['*', 'name', 'lambda_paramlist'],
+        ['*', 'lambda_paramlist', ',', 'lambda_kwparams'],
+        ['*', 'name', 'lambda_paramlist', ',', 'lambda_kwparams'],
+    ],
+    "lambda_kwparams": [
+        ['**', 'name'],
+        ['**', 'name', ','],
+    ],
+
+    "stmt": [
+        ['simple_stmt'],
+        ['compound_stmt'],
+    ],
+    "simple_stmt": [
+        ['small_stmt', 'small_stmt_list', 'NEWLINE'],
+        ['small_stmt', 'small_stmt_list', ';', 'NEWLINE'],
+    ],
+    "small_stmt_list": [
+        [],
+        [';', 'small_stmt', 'small_stmt_list'],
+    ],
+    "small_stmt": [
+        ['expr_stmt'],
+        ['assign_stmt'],
+        ['del_stmt'],
+        ['pass_stmt'],
+        ['flow_stmt'],
+        ['import_stmt'],
+        ['global_stmt'],
+        ['nonlocal_stmt'],
+        ['assert_stmt'],
+    ],
+    "expr_stmt": [
+        ['testlist_star_expr'],
+    ],
+    "assign_stmt": [
+        ['annassign'],
+        ['augassign'],
+        ['assign'],
+    ],
+
+    "annassign": [
+        ['testlist_star_expr', ':', 'test'],
+        ['testlist_star_expr', ':', 'test', '=', 'test'],
+    ],
+    "assign": [
+        ['testlist_star_expr', 'assign_follow_list'],
+    ],
+    "assign_follow_list": [
+        ['=', 'yield_expr'],
+        ['=', 'testlist_star_expr'],
+        ['=', 'yield_expr', 'assign_follow_list'],
+        ['=', 'testlist_star_expr', 'assign_follow_list'],
+    ],
+    "augassign": [
+        ['testlist_star_expr', 'augassign_op', 'yield_expr'],
+        ['testlist_star_expr', 'augassign_op', 'testlist'],
+    ],
+    "augassign_op": [
+        ['PLUSEQUAL'],
+        ['MINUSEQUAL'],
+        ['STAREQUAL'],
+        ['ATEQUAL'],
+        ['SLASHEQUAL'],
+        ['PERCENTEQUAL'],
+        ['AMPEREQUAL'],
+        ['VBAREQUAL'],
+        ['CIRCUMFLEXEQUAL'],
+        ['LEFTSHIFTEQUAL'],
+        ['RIGHTSHIFTEQUAL'],
+        ['DOUBLESTAREQUAL'],
+        ['DOUBLESLASHEQUAL'],
+    ],
+    "testlist_star_expr": [
+        ['test_or_star_expr'],
+        ['test_or_star_expr', ',', 'testlist_star_expr'],
+        ['test_or_star_expr', ','],
+    ],
+
+    "del_stmt": [
+        ['DEL', 'exprlist'],
+    ],
+    "pass_stmt": [
+        ['PASS'],
+    ],
+    "flow_stmt": [
+        ['break_stmt'],
+        ['continue_stmt'],
+        ['return_stmt'],
+        ['raise_stmt'],
+        ['yield_stmt'],
+    ],
+    "break_stmt": [
+        ['BREAK'],
+    ],
+    "continue_stmt": [
+        ['CONTINUE'],
+    ],
+    "return_stmt": [
+        ['RETURN'],
+        ['RETURN', 'testlist'],
+    ],
+    "yield_stmt": [
+        ['yield_expr'],
+    ],
+    "raise_stmt": [
+        ['RAISE'],
+        ['RAISE', 'test'],
+        ['RAISE', 'test', 'FROM', 'test'],
+    ],
+    "import_stmt": [
+        ['import_name'],
+        ['import_from'],
+    ],
+    "import_name": [
+        ['IMPORT', 'dotted_as_names'],
+    ],
+    "import_from": [
+        ['FROM', 'dotted_name', 'IMPORT', '*'],
+        ['FROM', 'dotted_name', 'IMPORT', '(', 'import_as_names', ')'],
+        ['FROM', 'dotted_name', 'IMPORT', 'import_as_names'],
+        ['FROM', 'dots', 'dotted_name', 'IMPORT', '*'],
+        ['FROM', 'dots', 'dotted_name', 'IMPORT', '(', 'import_as_names', ')'],
+        ['FROM', 'dots', 'dotted_name', 'IMPORT', 'import_as_names'],
+        ['FROM', 'dots', 'IMPORT', '*'],
+        ['FROM', 'dots', 'IMPORT', '(', 'import_as_names', ')'],
+        ['FROM', 'dots', 'IMPORT', 'import_as_names'],
+    ],
+    "dots": [
+        ['.'],
+        ['.', 'dots']
+    ],
+    "import_as_name": [
+        ['name'],
+        ['name', 'AS', 'name'],
+    ],
+    "dotted_as_name": [
+        ['dotted_name'],
+        ['dotted_name', 'as', 'name'],
+    ],
+    "import_as_names": [
+        ['import_as_name', 'import_as_name_list'],
+        ['import_as_name', 'import_as_name_list', ','],
+    ],
+    "import_as_name_list": [
+        [],
+        [',', 'import_as_name', 'import_as_name_list']
+    ],
+    "dotted_as_names": [
+        ['dotted_as_name', 'dotted_as_name_list'],
+    ],
+    "dotted_as_name_list": [
+        [],
+        [',', 'dotted_as_name', 'dotted_as_name_list'],
+    ],
+    "dotted_name": [
+        ['name', 'dotted_name_list'],
+    ],
+    "dotted_name_list": [
+        [],
+        ['.', 'name', 'dotted_name_list'],
+    ],
+    "global_stmt": [
+        ['GLOBAL', 'name', 'comma_name_list'],
+    ],
+    "nonlocal_stmt": [
+        ['NONLOCAL', 'name', 'comma_name_list'],
+    ],
+    "comma_name_list": [
+        [],
+        [',', 'name', 'comma_name_list'],
+    ],
+    "assert_stmt": [
+        ['ASSERT', 'test'],
+        ['ASSERT', 'test', ',', 'test']
+    ],
+
+    "compound_stmt": [
+        ['if_stmt'],
+        ['while_stmt'],
+        ['for_stmt'],
+        ['try_stmt'],
+        ['match_stmt'],
+        ['with_stmt'],
+        ['funcdef'],
+        ['classdef'],
+        ['decorated'],
+        ['async_stmt'],
+    ],
+    "async_stmt": [
+        ['ASYNC', 'funcdef'],
+        ['ASYNC', 'with_stmt'],
+        ['ASYNC', 'for_stmt'],
+    ],
+    "if_stmt": [
+        ['IF', 'test', ':', 'suite', 'elifs'],
+        ['IF', 'test', ':', 'suite', 'elifs', 'ELSE', ':', 'suite'],
+    ],
+    "elifs": [
+        [],
+        ['ELIF', 'test', ':', 'suite', 'elifs'],
+    ],
+    "while_stmt": [
+        ['WHILE', 'test', ':', 'suite'],
+        ['WHILE', 'test', ':', 'suite', 'ELSE', ':', 'suite'],
+    ],
+    "for_stmt": [
+        ['for', 'exprlist', 'IN', 'testlist', ':', 'suite'],
+        ['for', 'exprlist', 'IN', 'testlist', ':', 'suite', 'ELSE', ':', 'suite'],
+    ],
+    "try_stmt": [
+        ['TRY', ':', 'suite', 'except_clauses'],
+        ['TRY', ':', 'suite', 'except_clauses', 'ELSE', ':', 'suite'],
+        ['TRY', ':', 'suite', 'except_clauses', 'finally'],
+        ['TRY', ':', 'suite', 'except_clauses', 'ELSE', ':', 'suite', 'finally'],
+        ['TRY', ':', 'suite', 'finally'],
+    ],
+    "finally": [
+        ['FINALLY', ':', 'suite'],
+    ],
+    "except_clauses": [
+        ['except_clause'],
+        ['except_clause', 'except_clauses'],
+    ],
+    "except_clause": [
+        ['EXCEPT', ':', 'suite'],
+        ['EXCEPT', 'test', ':', 'suite'],
+        ['EXCEPT', 'test', 'AS', 'name', ':', 'suite'],
+    ],
+
+
+    "with_stmt": [
+        ['WITH', 'with_items', ':', 'suite'],
+    ],
+    "with_items": [
+        ['with_item', 'with_item_list'],
+    ],
+    "with_item_list": [
+        [],
+        [',', 'with_item', 'with_item_list'],
+    ],
+    "with_item": [
+        ['test'],
+        ['test', 'AS', 'name']
+    ],
+
+    "match_stmt": [
+        ['MATCH', 'test', ':', 'NEWLINE', 'cases'] # TODO MISSING INDENT DEDENT
+    ],
+    "cases": [
+        ['case'],
+        ['case', 'cases'],
+    ],
+    "case": [
+        ['CASE', 'pattern', ':', 'suite'],
+        ['CASE', 'pattern', 'IF', 'test', ':', 'suite'],
+    ],
+
+    "pattern": [
+        ['sequence_item_pattern', ',',  '_sequence_pattern'],
+        ['as_pattern'],
+    ],
+    "as_pattern": [
+        ['or_pattern', 'AS', 'name'],
+    ],
+    "or_pattern": [
+        ['closed_pattern', 'closed_pattern_list'],
+    ],
+    "closed_pattern_list": [
+        [],
+        ['|', 'closed_pattern', 'closed_pattern_list'],
+    ],
+    "closed_pattern": [
+        ['literal_pattern'],
+        ['NAME'],
+        ['_'],
+        ['attr_pattern'],
+        ['(', 'as_pattern', ')'],
+        ['[', '_sequence_pattern', ']'],
+        ['(', ')'],
+        ['(', 'sequence_item_pattern', ',',  '_sequence_pattern', ')'],
+        ['{', '}'],
+        ['{', 'mapping_item_pattern', 'mapping_item_list', '}' ],
+        ['{', 'mapping_item_pattern', 'mapping_item_list', ',', '}' ],
+        ['{', '**', 'NAME', '}'],
+        ['{', '**', 'NAME', ',', '}'],
+        ['{', 'mapping_item_pattern', 'mapping_item_list', ',', '**', 'NAME', '}'],
+        ['{', 'mapping_item_pattern', 'mapping_item_list', ',', '**', 'NAME', ',', '}'],
+        ['class_pattern'],
+    ],
+    "mapping_item_list": [
+        [],
+        [',', 'mapping_item_pattern', 'mapping_item_list'],
+    ],
+
+    "literal_pattern": [
+        ['inner_literal_pattern'],
+    ],
+    "inner_literal_pattern": [
+        ['NONE'],
+        ['TRUE'],
+        ['FALSE'],
+        ['STRING'],
+        ['number'],
+    ],
+
+    "attr_pattern": [
+        ['NAME', '.', 'NAME', 'dot_name_list'],
+    ],
+    "dot_name_list": [
+        [],
+        ['.', 'NAME', 'dot_name_list'],
+    ],
+    "name_or_attr_pattern": [
+        ['NAME', 'dot_name_list'],
+    ],
+
+    "mapping_item_pattern": [
+        ['literal_pattern', ':', 'as_pattern'],
+        ['attr_pattern', ':', 'as_pattern'],
+    ],
+
+    "_sequence_pattern": [
+        [],
+        ['sequence_item_pattern', 'sequence_item_list'],
+        ['sequence_item_pattern', 'sequence_item_list', ','],
+    ],
+    "sequence_item_list": [
+        [],
+        [',', 'sequence_item_pattern', 'sequence_item_list'],
+    ],
+    "sequence_item_pattern": [
+        ['as_pattern'],
+        ['*', 'NAME'],
+    ],
+
+    "class_pattern": [
+        ['name_or_attr_pattern', '(', ')'],
+        ['name_or_attr_pattern', '(', 'arguments_pattern', ')'],
+        ['name_or_attr_pattern', '(', 'arguments_pattern', ',', ')'],
+    ],
+    "arguments_pattern": [
+        ['pos_arg_pattern'], 
+        ['pos_arg_pattern', ',', 'keyws_arg_pattern'],
+        ['keyws_arg_pattern'],
+    ],
+    "post_arg_pattern": [
+        ['as_pattern', 'as_pattern_list'],
+    ],
+    "as_pattern_list": [
+        [],
+        [',', 'as_pattern', 'as_pattern_list'],
+    ],
+    "keyws_arg_pattern": [
+        ['keyw_arg_pattern', 'keyw_arg_list'],
+    ],
+    "keyw_arg_list": [
+        [],
+        [',', 'keyw_arg_pattern', 'keyw_arg_list'],
+    ],
+    "keyw_arg_pattern": [
+        ['NAME', '=', 'as_pattern'],
+    ],
 }
 
 reserved = {
         'False': 'FALSE',
+        'await': 'AWAIT',
 		'else': 'ELSE',
 		'import': 'IMPORT',
 		'pass': 'PASS',
@@ -34,6 +505,7 @@ reserved = {
 		'global': 'GLOBAL',
 		'not': 'NOT',
 		'with': 'WITH',
+        'async': 'ASYNC',
 		'elif': 'ELIF',
 		'if': 'IF',
 		'or': 'OR',
@@ -41,6 +513,7 @@ reserved = {
         'match': 'MATCH',
         'case': 'CASE',
         'type': 'TYPE',
+        '_': '_',
     }
 
 class PythonLexer(object):
@@ -118,7 +591,7 @@ class PythonLexer(object):
     t_ELLIPSIS		    = r'\.\.\.'
     t_COLONEQUAL		= r':='
 
-    literals = "()[]:,;+-*/|&<>=.%{}~^@!_"
+    literals = "()[]:,;+-*/|&<>=.%{}~^@!"
 
     def t_NAME(self,t):
         r'[^\W\d]\w*'
@@ -199,6 +672,7 @@ if __name__ == "__main__":
                 test
                 """
 
+                . ...
             '''
     l.input(test)     # Test it
 
